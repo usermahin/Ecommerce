477cf08afde030e4aeb11df7bc0f9f16863a220d
usermahin <almassadaf200@gmail.com>
2023-01-31 16:15:55 +0530

    College Project

---
+++ ecommerce/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <marquee><strong>Copyright &copy; 2023 Brought to You By <a href="https://code-projects.org/">  Almas and saba,CSE department</a></strong></marquee>
+    </div>
+</footer>
